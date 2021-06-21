fa5dff5ac96fc511baa80a401c9eb7b6cf192ad0
Diolg <olga.diakoff@gmail.com>
2021-06-21 11:02:48 +0000

    Add products views and templates

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def all_sessions(request):
+    """ A view to return all sessions and search queries """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/sessions.html', context) 
+
+
