1c94ff37fccab2a84b8569337fcda71df084801c
Diolg <olga.diakoff@gmail.com>
2021-06-21 21:13:03 +0000

    Add session details functionality

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_sessions, name='products')
+    path('', views.all_sessions, name='products'),
+    path('<product_id>', views.session_detail, name='session_detail'),
 ]
