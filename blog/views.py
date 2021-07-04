134a15f776febd04d94e4d960f43352c34b90ed5
Diolg <olga.diakoff@gmail.com>
2021-07-04 22:26:08 +0000

    Update blog.html template and create post_detial.html template

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,12 @@ from .models import Post
 
 # Create your views here.
 def blog(request):
+    posts = Post.objects.all()
    
-    return render(request, 'blog/blog.html')
+    return render(request, 'blog/blog.html', {'posts': posts})
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug=slug)
+
+    return render(request, 'blog/post_detail.html', {'post': post})
 
