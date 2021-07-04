ca64e46b362703e8998afb7d8c364440c7ffff0d
Diolg <olga.diakoff@gmail.com>
2021-07-04 22:52:05 +0000

    Create form.py and post_detail.html, add comment functionality

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from .forms import CommentForm
 from .models import Post
 
 # Create your views here.
@@ -12,5 +13,17 @@ def blog(request):
 def post_detail(request, slug):
     post = Post.objects.get(slug=slug)
 
-    return render(request, 'blog/post_detail.html', {'post': post})
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('post_detail', slug=post.slug)
+    else: 
+        form = CommentForm()
+
+    return render(request, 'blog/post_detail.html', {'post': post, 'form': form})
 
