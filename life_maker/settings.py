feab1e8c9fffda633ca9fe257fd32ee54a1643b6
Diolg <olga.diakoff@gmail.com>
2021-07-13 20:22:34 +0000

    Remove secret key and set debug

--- life_maker/settings.py
+++ life_maker/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sy-!4&l=%u4)72m%6mxme3!-rtd)ywfcvqa+@fhcwvp$dwa_#n'
+SECRET_KEY = os.environ.get('SEKRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['life-maker.herokuapp.com', 'localhost']
 
