99772b8f699f40adb258f4971fbb8234e049723c
Diolg <olga.diakoff@gmail.com>
2021-07-15 09:36:42 +0000

    login/reg refactor and change color of the blog header

--- life_maker/settings.py
+++ life_maker/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '(d0e+t^si-jwh&g@1n)2a&x#0apoj@5vau7$b8^=+=r5biv4c5')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
